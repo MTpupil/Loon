a3cd56afb5cb7a016dad94c09c7108aee9443e9c
mtpupil <970632312@qq.com>
2025-05-18 15:15:48 +0800

    refactor(wxstep.js): 统一参数获取方式
    将$argument.arg1方法调用改为直接访问$argument对象属性，提高代码可读性和一致性

--- Task/wxstep.js
+++ Task/wxstep.js
@@ -5,13 +5,13 @@
  * 
  */
 
-let notice = $argument.arg1("wxstepNotice");
-let user = $argument.arg1("wxstepUser");
-let pwd = $argument.arg1("wxstepPwd");
-let wxstepRandom = $argument.arg1("wxstepRandom");
-let stepMax = $argument.arg1("wxstepStepMax");
-let stepMin = $argument.arg1("wxstepStepMin");
-let wxstep = $argument.arg1("wxstepnum");
+let notice = $argument.wxstepNotice
+let user = $argument.wxstepUser
+let pwd = $argument.wxstepPwd
+let wxstepRandom = $argument.wxstepRandom
+let stepMax = $argument.wxstepStepMax
+let stepMin = $argument.wxstepStepMin
+let wxstep = $argument.wxstepnum
 
 // 参数校验
 if (!user || !pwd) {
