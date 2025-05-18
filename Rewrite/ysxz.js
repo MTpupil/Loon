e19a195afc6762b541f52cfcf1f2e8501684eef4
mtpupil <970632312@qq.com>
2025-05-18 12:08:32 +0800

    feat(云上写作): 添加破解SVIP功能并更新脚本路径
    在Rewrite/ysxz.js中添加了破解云上写作SVIP的功能，并更新了Plugins/MTpupilRewrite.plugin中的脚本路径，以确保功能正常运行。

---
+++ Rewrite/ysxz.js
@@ -0,0 +1,23 @@
+/**
+ * 云上写作破解svip
+ * 公众号：木瞳科技Pro
+ * [MITM]
+ * hostname = *.yunshangxiezuo.com
+ * 
+ */
+
+
+const SCRIPT_NAME = '云上写作';
+
+const user = /^https?:\/\/www\.yunshangxiezuo\.com\/api\/getAuthenticate_2/;
+
+if (user.test($request.url)) {
+    let obj = JSON.parse($response.body);
+    obj.data.user["name"] = "木瞳科技Pro";
+    obj.data.user["email"] = "破解成功，解锁所有功能";
+    obj.data.user["vip"] = 1;
+    obj.data.user["vip_forever"] = 1;
+    obj.data.user["vip_last"] = "2099-12-13 00:00:00";
+    let body = JSON.stringify(obj);
+    $done({ body })
+}
