58f8ca14392497014ba6ebf825062b00e5825256
mtpupil <970632312@qq.com>
2025-05-18 16:16:36 +0800

    feat: 新增wifi到期提醒功能
    添加了一个新的用户脚本，用于监控wifi到期时间和剩余流量，并在接近到期或流量不足时发送通知。该脚本通过HTTP请求获取设备状态和流量信息，并根据条件触发提醒。

---
+++ Task/wifiTracker.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         HuaHai IoT Request
+// @namespace    
+// @version      0.1
+// @description  wifi到期提醒
+// @author       木瞳
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+const cardNo = "70032342060810";
+
+
+let option = {
+    url: `http://iot-wx.lktweixin.cn/HuaHai/index?cardNo=70032342060810`,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+};
+
+
+$httpClient.get(option, (error, response, data) => {
+    let body = JSON.parse(response.body);
+    let msg = body.msg
+    if (msg.includes("OK")) {
+        let data = body.data;
+        let serveValidDate = new Date(data.serveValidDate);
+        let now = new Date();
+        let diffTime = Math.abs(serveValidDate - now);
+        let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        let surplusFlow = parseFloat(data.surplusFlowText);
+        let deviceMonthFlow = parseFloat(data.deviceMonthFlow);
+        let flowPercentage = (surplusFlow / deviceMonthFlow) * 100;
+
+        if (data.deviceState === '在线') {
+            if (diffDays > -2 && (diffDays <= 3 || flowPercentage < 10)) {
+                let message = `到期时间剩余 ${diffDays} 天，剩余流量百分比为 ${flowPercentage.toFixed(2)}%`;
+                $notification.post("wifi", "到期或流量不足提醒", message);
+            }
+        }
+    } else {
+        $notification.post("wifi", "数据获取失败", msg);
+    }
+
+    $done();
+}, error => {
+    console.log(error);
+    $done();
+})
