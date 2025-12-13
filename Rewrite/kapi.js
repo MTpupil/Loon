6cd3ec77ce6ad32907593535e8705d2d812a8fd8
杨超越 <62123934+MTpupil@users.noreply.github.com>
2025-12-13 19:20:44 +0800

    创建 kapi.js

---
+++ Rewrite/kapi.js
@@ -0,0 +1,27 @@
+/**
+ * 卡皮记账解锁修改会员
+ * 公众号：木瞳科技Pro
+ * 
+ * [MITM]
+ * hostname = api.heylumi.cn
+ * 
+ * 
+ */
+
+const SCRIPT_NAME = '咔皮记账';
+const vip = /^https?:\/\/api.heylumi.cn\/note\/note-api\/user\/account\/status/;
+
+
+
+
+if (vip.test($request.url)) {
+    let obj = JSON.parse($response.body);
+
+    obj.data.memberLevel = "VIP";
+    obj.data.vipLevel = "VIP";
+    obj.data.endTime = "2099-12-31 23:23:59";
+
+    obj.data.coins = 999999;
+    let body = JSON.stringify(obj);
+    $done({ body })
+}
