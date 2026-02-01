dabce6c99c9701df10d6053323bcf7b009d4306e
杨超越 <62123934+MTpupil@users.noreply.github.com>
2026-02-01 16:49:29 +0800

    创建 yjb.js

---
+++ Rewrite/yjb.js
@@ -0,0 +1,22 @@
+/**
+ * 养基宝破解vip
+ * 公众号：木瞳科技Pro
+ * [MITM]
+ * hostname = *.yangjibao.com
+ * 
+ * 
+ */
+const url = /^https?:\/\/wx\.yangjibao\.com\/wxapi\/account/;
+
+if (url.test($request.url)) {
+    let body = JSON.parse($response.body);
+    let data = body.data
+    data.open_free_vip_sign = true;
+    data.name = "木瞳科技Pro"
+    data.is_pay = true
+    data.vip_expiry_date = "终身会员"
+    data.subscribe_status = 1
+    data.vip_label = true
+    data.show_bkxh = true
+    $done({ body: JSON.stringify(body) })
+}
